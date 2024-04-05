6941c06dc9b212663c565a00b218366da882a047
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2024-04-05 23:05:45 +0530

    fix Error

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import store from '../store/store.js'
 import SignUpPage from "../Pages/SignUpPage.jsx"
 import ChoosAvatarPage from '../Pages/ChoosAvatarPage.jsx'
 import ChoosRolePage from "../Pages/ChoosRolePage.jsx"
@@ -40,8 +38,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    
     <RouterProvider router={router}/>
-    </Provider>
+    
   </React.StrictMode>,
 )
